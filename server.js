8eada66413fa94f955bf091e191312843770cf41
Awe03 <106132059+Awe03@users.noreply.github.com>
2022-11-26 21:00:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,11 @@
 const host = process.env.HOST || '0.0.0.0';
+// Listen on a specific port via the PORT environment variable
 const port = process.env.PORT || 4000;
 
 const cors_proxy = require('cors-anywhere');
 cors_proxy.createServer({
-    originWhitelist: [],
+    originWhitelist: [], // Allow all origins
     removeHeaders: ['cookie', 'cookie2']
 }).listen(port, host, function() {
     console.log('Running CORS Anywhere on ' + host + ':' + port);
 });
-const express = require('express');
-const request = require('request');
-const serverless = require('serverless-http');
-const app = express();
-app.use('/', (req, res) => {
-    const url = 'http://localhost:4000' + req.url;
-    req.pipe(request(url)).pipe(res);
-});
-
-app.listen(3000);
-
-module.exports = app;
-module.exports.handler = serverless(app);
