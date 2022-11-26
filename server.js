faf7d67a6f0f8e85c440c78c575674abe0318dc4
Awe03 <106132059+Awe03@users.noreply.github.com>
2022-11-26 20:52:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,3 +9,15 @@ cors_proxy.createServer({
 }).listen(port, host, function() {
     console.log('Running CORS Anywhere on ' + host + ':' + port);
 });
+
+//make express listen to 4000 on 3000 and every path will be called to 4000
+//for example, if i call localhost:3000/api/whatever, it will be called to localhost:4000/api/whatever
+const express = require('express');
+const request = require('request');
+const app = express();
+app.use('/', (req, res) => {
+    const url = 'http://localhost:4000' + req.url;
+    req.pipe(request(url)).pipe(res);
+});
+
+app.listen(3000);
